c31a1e025b6af312e72edbb7eecc250b4f52c48c
[AI5/1반] 이태의 <wishty@gmail.com>
2022-11-24 19:55:02 +0900

    [BE/REFACT] eslint, prettier 개선, airbnb 컨벤션 적용-수정1

--- back/src/sell/sell.controller.ts
+++ back/src/sell/sell.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { SellService } from './sell.service';
 import { CreateSellDto } from './dto/create-sell.dto';
 import { UpdateSellDto } from './dto/update-sell.dto';
