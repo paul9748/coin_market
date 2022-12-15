a5871ccca0d20a045a2e637150a2318dad9ebd6f
minkyu <tour00389@gmail.com>
2022-12-15 19:38:28 +0900

    [FE/FEAT] footer 내용 기입

--- front/src/components/UI/Footer.js
+++ front/src/components/UI/Footer.js
@@ -1,17 +1,91 @@
 import styled from 'styled-components';
-
+import logo from '../../assets/images/coin_market(2).png';
 function Footer() {
-  return <StyledFooter></StyledFooter>;
+  return (
+    <StyledFooter>
+      <AllContents>
+        <LogoDiv>
+          <LogoImg src={logo}></LogoImg>
+        </LogoDiv>
+        <CompanyDetail>
+          <StyledPtag style={{ textAlign: 'center' }}>
+            상호 : (주)코인마켓 | 대표자 : 엘리스
+          </StyledPtag>
+          <StyledPtag style={{ textAlign: 'center' }}>
+            사업자등록번호 : 000-00-00000
+          </StyledPtag>
+          <StyledPtag style={{ textAlign: 'center' }}>
+            연락처 : 000-0000-0000 | 팩스 : 000-000-0000 | 이메일 : coinmarket@elice.com
+          </StyledPtag>
+          <StyledPtag style={{ textAlign: 'center' }}>
+            주소 : 서울특별시 송파구 올림픽로 300 롯데월드타워 117F
+          </StyledPtag>
+          <div style={{ marginTop: '50px' }}>
+            <StyledPtag style={{ textAlign: 'center' }}>
+              이용약관 <strong>개인정보처리방침</strong>
+            </StyledPtag>
+            <StyledPtag style={{ textAlign: 'center' }}>Copyright © 코인마켓</StyledPtag>
+          </div>
+        </CompanyDetail>
+      </AllContents>
+    </StyledFooter>
+  );
 }
 
 export default Footer;
 
 const StyledFooter = styled.footer`
   background: rgb(51, 51, 51);
-  height: 100px;
-  margin-top: auto;
+  height: auto;
   min-width: 600px;
-  @media (max-width: 600px) {
+  margin: auto;
+
+  @media (max-width: 700px) {
     min-width: 440px;
+  }
+`;
+const AllContents = styled.div`
+  display: flex;
+  text-align: center;
+  vertical-align: middle;
+  margin: auto;
+`;
+const LogoDiv = styled.div`
+  display: table;
+  text-align: 'center';
+  padding: 10px;
+  margin-top: auto;
+  margin-bottom: auto;
+`;
+
+const LogoImg = styled.img`
+  height: 150px;
+  margin: 50px;
+  vertical-align: middle;
+  @media (max-width: 700px) {
+    height: 100px;
+    margin: auto;
+    padding-left: 15px;
+  }
+`;
+
+const CompanyDetail = styled.div`
+  text-align: 'center';
+  /* align-self: 'center' */
+  padding: 80px 100px 50px 100px;
+  color: white;
+  margin: auto;
+  @media (max-width: 700px) {
+    padding: 40px 50px 25px 50px;
+  }
+`;
+
+const StyledPtag = styled.p`
+  text-align: 'center';
+  line-height: 30px;
+  font-size: 20px;
+  @media (max-width: 700px) {
+    line-height: 19px;
+    font-size: 15px;
   }
 `;
