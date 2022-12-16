87da83a0a095c5971488efc6ed6c5edbc42ee649
paul9748 <paul9748@hotmail.co.kr>
2022-12-16 22:06:39 +0900

    [FRONT]판매 이미지 URL 추가및 새창

--- front/src/components/Table.js
+++ front/src/components/Table.js
@@ -1,14 +1,12 @@
 /* eslint-disable react/jsx-key */
 import styled from 'styled-components';
 import { useTable } from 'react-table';
-
 function Table({ columns, data }) {
   // Use the state and functions returned from useTable to build your UI
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
     columns,
     data,
   });
-
   // Render the UI for your table
   return (
     <StyledTable {...getTableProps()}>
@@ -31,9 +29,11 @@ function Table({ columns, data }) {
                   <td
                     {...cell.getCellProps()}
                     onClick={() => {
-                      console.log(cell.value);
-                      console.log(cell);
-                      self.Text = '테스트';
+                      // console.log(cell.value);
+                      console.log(location);
+                      if (cell.column['Header'] == '이미지' && cell.value != null) {
+                        window.open(window.location.hostname + ':3000/img/' + cell.value);
+                      }
                     }}>
                     {cell.render('Cell')}
                   </td>
