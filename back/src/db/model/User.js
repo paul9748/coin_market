c20de244b31f3a66c88f440a911c820dac00f7b8
minkyu <tour00389@gmail.com>
2023-01-02 00:37:53 +0900

    [BE/REFACT] user model update

--- back/src/db/model/User.js
+++ back/src/db/model/User.js
@@ -1,15 +1,13 @@
 import bcrypt from "bcrypt";
 import { db } from "../db";
 import { v4 } from "uuid";
-import { nextTick } from "process";
-import { Prisma } from "@prisma/client";
 
 class User {
   static async createUserByEmailAndPassword(data) {
     data.password = bcrypt.hashSync(data.password, 12);
     data.id = v4();
 
-    const newUser = await db.user.create({
+    const newUser = db.user.create({
       data,
     });
     return newUser;
