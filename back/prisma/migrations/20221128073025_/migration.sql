5b3b9c7e3e2a3794c5b7efb4dbdc969f6857291f
minkyu <tour00389@gmail.com>
2023-01-01 23:43:09 +0900

    [BE/REFACT] delete back folder

--- back/prisma/migrations/20221128073025_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE `deal_detail` MODIFY `company` VARCHAR(191) NULL,
-    MODIFY `delivery_number` VARCHAR(191) NULL;
--- back/src/db/db.js
+++
@@ -1,5 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const db = new PrismaClient();
-
-module.exports = { db };
